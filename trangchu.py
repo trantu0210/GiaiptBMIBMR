dba95f82817a278b036e66019119f3e3de001461
Ngoc Tu <99663833+trantu0210@users.noreply.github.com>
2024-12-20 13:36:13 +0700

    Update trangchu.py

--- trangchu.py
+++ trangchu.py
@@ -40,17 +40,17 @@ class MainMenu(ft.Container):
                                 padding=padding.only(left=25, right=25),
                                 content=ft.ResponsiveRow(                                
                                     controls=[
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" Bậc 1 ", on_click=lambda _: self.page.go("/bac1"))]),
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" Bậc 2 ", on_click=lambda _: self.page.go("/bac2"))]),
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  Bậc 1  ", on_click=lambda _: self.page.go("/bac1"))]),
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  Bậc 2  ", on_click=lambda _: self.page.go("/bac2"))]),
                                         ft.Row(controls=[ft.Text("", size=0)]),
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" Bậc 3 ", on_click=lambda _: self.page.go("/bac3"))]),
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" Bậc 4 ", on_click=lambda _: self.page.go("/bac4"))]),
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  Bậc 3  ", on_click=lambda _: self.page.go("/bac3"))]),
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  Bậc 4  ", on_click=lambda _: self.page.go("/bac4"))]),
                                         ft.Row(controls=[ft.Text("", size=0)]),
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" MT 1 ", on_click=lambda _: self.page.go("/maytinh1"))]),
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" MT 2 ", on_click=lambda _: self.page.go("/maytinh2"))]),
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  MT 1  ", on_click=lambda _: self.page.go("/maytinh1"))]),
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  MT 2  ", on_click=lambda _: self.page.go("/maytinh2"))]),
                                         ft.Row(controls=[ft.Text("", size=0)]),
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" MT 3 ", on_click=lambda _: self.page.go("/maytinh3"))]),
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" MT 4 ", on_click=lambda _: self.page.go("/maytinh4"))]),                                        
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  MT 3  ", on_click=lambda _: self.page.go("/maytinh3"))]),
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  MT 4  ", on_click=lambda _: self.page.go("/maytinh4"))]),                                        
                                         ft.Row(controls=[ft.Text("", size=0)])   
                                     ]
                                 )
@@ -59,11 +59,11 @@ class MainMenu(ft.Container):
                                 padding=padding.only(left=25, right=25),
                                 content=ft.ResponsiveRow(                                
                                     controls=[
-                                        ft.Column(col={"xs":12}, controls=[ ElevatedButton(" Hệ pt 2 ẩn ", on_click=lambda _: self.page.go("/hept2an"))]), 
+                                        ft.Column(col={"xs":12}, controls=[ ElevatedButton("  Hệ pt 2 ẩn  ", on_click=lambda _: self.page.go("/hept2an"))]), 
                                         ft.Row(controls=[ft.Text("", size=0)]),
-                                        ft.Column(col={"xs":12}, controls=[ ElevatedButton(" Hệ pt 3 ẩn ", on_click=lambda _: self.page.go("/hept3an"))]),
+                                        ft.Column(col={"xs":12}, controls=[ ElevatedButton("  Hệ pt 3 ẩn  ", on_click=lambda _: self.page.go("/hept3an"))]),
                                         ft.Row(controls=[ft.Text("", size=0)]), 
-                                        ft.Column(col={"xs":12}, controls=[ ElevatedButton(" Hướng dẫn sử dụng ", on_click=lambda _: self.page.go("/huongdansudung"))]),                                                                            
+                                        ft.Column(col={"xs":12}, controls=[ ElevatedButton("  Hướng dẫn sử dụng  ", on_click=lambda _: self.page.go("/huongdansudung"))]),                                                                            
                                         ft.Row(controls=[ft.Text("", size=0)])
                                     ]
                                 )
@@ -76,10 +76,10 @@ class MainMenu(ft.Container):
                                 padding=padding.only(left=25, right=25),
                                 content=ft.ResponsiveRow(                                
                                     controls=[
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" BMI ", on_click=lambda _: self.page.go("/bmi"))]),
-                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton(" BMR ", on_click=lambda _: self.page.go("/bmr"))]),
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  BMI  ", on_click=lambda _: self.page.go("/bmi"))]),
+                                        ft.Column(col={"xs":6}, controls=[ ElevatedButton("  BMR  ", on_click=lambda _: self.page.go("/bmr"))]),
                                         ft.Row(controls=[ft.Text("", size=0)]),
-                                        ft.Column(col={"xs":12}, controls=[ ElevatedButton(" Bảng BMI ", on_click=lambda _: self.page.go("/bangdulieu"))]),
+                                        ft.Column(col={"xs":12}, controls=[ ElevatedButton("  Bảng BMI  ", on_click=lambda _: self.page.go("/bangdulieu"))]),
                                         ft.Row(controls=[ft.Text("", size=0)]),
                                         ft.Row(controls=[ft.Text("", size=0)])
                                     ]
